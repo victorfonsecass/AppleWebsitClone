ed22b16d01986d671fa71e15801fa67525c98206
victor fonseca <victorfonsecass@outlook.com>
2024-08-13 13:07:42 -0300

    video caurosel animated

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -2,6 +2,8 @@ import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
 import { rightImg, watchImg } from "../utils"
 
+import VideoCarousel from "./VideoCarousel"
+
 const Highlights = () => {
   useGSAP(() => {
     gsap.to('#title', { opacity: 1, y: 0})
@@ -24,7 +26,8 @@ const Highlights = () => {
             </p>
           </div>
         </div>
-        
+
+        <VideoCarousel/>
       </div>
     </section>
   )
