2cdab9df3f518a0e247f21f42154a129276758b0
victor fonseca <victorfonsecass@outlook.com>
2024-08-13 05:22:12 -0300

    basic animations hero done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,13 +13,18 @@ const Hero = () => {
     } else {
       setVideoSrc(heroVideo)
     }
-
+  }
     useEffect(() => {
       window.addEventListener('resize', handleVideoSrcSet);
-    },)
-  }
+
+      return () => {
+        window.removeEventListener('resize', handleVideoSrcSet);
+      }
+    },[])
+
   useGSAP(() => {
     gsap.to('#hero',{ opacity:1, delay: 1.5 })
+    gsap.to('#cta',{ opacity:1, y:-50, delay: 2 })
   },[])
   return (
     <section className="w-full nav-height bg-black relative">
@@ -31,6 +36,10 @@ const Hero = () => {
             <source src={videoSrc} type="video/mp4"/>
           </video>
         </div>
+      </div>
+      <div id="cta" className="flex flex-col items-center opacity-0  translate-y-20">
+        <a href="#highlights" className="btn">Buy</a>
+        <p className="font-nrm text-xl">From $199/month or $999</p>
       </div>
     </section>
   )
