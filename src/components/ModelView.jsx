fb8a144d027d28e64e3bba5ec4d9d08868ea40c6
victor fonseca <victorfonsecass@outlook.com>
2024-08-17 13:42:07 -0300

    Implement OrbitControls and dynamic group with iPhone component
    - Added  with customized settings for rotation speed and target position. Disabled zoom and pan functionalities.
    - Configured  with dynamic name and position based on the  prop.
    - Integrated  component within a  fallback, adjusting scale based on  and passing  and  props.
    - Added  component as fallback for async component loading.

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -18,13 +18,26 @@ const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, si
       <PerspectiveCamera makeDefault position={[0, 0, 4]} />
 
       <Lights />
-      <Suspense fallback={<Loader />}>
-        <IPhone
-          scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
-          item={item}
-          size={size}
-          />
-      </Suspense>
+
+      <OrbitControls
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0 ,0)}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+      />
+
+      <group ref={groupRef} name={`${index === 1} ? 'small' : 'large`} position={[0, 0 ,0]}>
+        <Suspense fallback={<Loader />}>
+          <IPhone
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            item={item}
+            size={size}
+            />
+        </Suspense>
+      </group>
     </View>
   )
 }
