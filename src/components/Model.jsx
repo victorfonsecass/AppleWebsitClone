0749524955569bc3ca338c42324448586c04db85
victor fonseca <victorfonsecass@outlook.com>
2024-08-17 11:23:49 -0300

    header animated

---
+++ src/components/Model.jsx
@@ -0,0 +1,16 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const Model = () => {
+    useGSAP(() => {
+    gsap.to('#heading', { y: 0, opacity: 1 })
+  }, []);
+  <section className="common-padding">
+    <div className="screen-max-width">
+      <h1 id="heading" className="section-heading">
+        Take a closer look.
+      </h1>
+    </div>
+  </section>
+}
+export default Model
