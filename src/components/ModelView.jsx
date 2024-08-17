0b44bcdc3d191682b51dc91c4681c0266bfbbe90
victor fonseca <victorfonsecass@outlook.com>
2024-08-17 13:19:04 -0300

    - ModelView component that sets up a 3D view with dynamic positioning based on the  prop. - Added 'ambientLight' with an intensity of 0.3 for basic scene lighting. - Configured 'PerspectiveCamera' with a default position to simulate a human eye view.

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,10 +1,19 @@
+import { PerspectiveCamera, View } from "@react-three/drei"
 
-
-const ModelView = () => {
+{/*Make changes to the 3D models will display on screen */}
+const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, size, item }) => {
   return (
-    <div>
-      ModelView
-    </div>
+    <View
+      index={index}
+      id={gsapType}
+      className={`w-full h-full absolute ${index === 2 ? 'right-[-100%]' : ''}`}
+    >
+      {/* Ambient Light */}
+      <ambientLight intensity={0.3} />
+
+      {/* Camera that simulates human eye */}
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+    </View>
   )
 }
 
